380a664a215e80ed5716d5ca45d134ccfd6c14a5
Fabrizio Usai <development@mensa.be>
2020-11-13 05:30:53 +0100

    temp vue config

--- vue.config.js
+++ vue.config.js
@@ -8,28 +8,84 @@ module.exports = {
     devServer: {
         proxy: 'https://localhost:8100'
     },
-    outputDir: 'src/main/webapp/temp',
+    // For build only
+    outputDir: publicDir + '/tmp',
+
     chainWebpack: config => {
-        config
-            .plugin('html')
-            .tap(args => {
-                args[0].template = vueWorkDir + '/index.xhtml'
-                return args
-            }),
-            // configure public dir for static file access
-            // this is needed for npm serve, otherwise the index.html
+
+        const inlineLimit = 10000;
+
+        if (process.env.NODE_ENV === 'production') {
+            // configure public dir for ignoring files in src/main/webapp that don't need vue processing (serving)
+            // this is needed for npm build
             config
                 .plugin('copy')
                 .use(require('copy-webpack-plugin'), [[{
                     from: path.resolve(__dirname, publicDir),
-                    ignore: ['vue/**/*']
-                }]]),
+                    ignore: ['index.xhtml', 'test.xhtml', '.gitignore', 'design/**/*', 'authentication/**/*', 'member/**/*', 'META-INF/**/*', 'WEB-INF/**/*']
+                }]])
+        } else {
+            // configure public dir for static file access
+            // this is needed for npm serve, otherwise the index.xhtml won't pick up the files in the strc/main/webapp/design folder
+            config
+                .plugin('copy')
+                .use(require('copy-webpack-plugin'), [[{
+                    from: path.resolve(__dirname, vueWorkDir)
+                }]])
+        }
+        config
+            .plugin('html')
+            .tap(args => {
+                // Read the index.xhtml from the vue work dir
+                args[0].template = vueWorkDir + '/index.xhtml'
+                // Copy it to the output dir
+                args[0].filename = 'index.xhtml'
+                return args
+            }),
+
             config
                 .entry('app')
                 .clear()
                 .add('./' + vueWorkDir + '/main.js')
                 .end(),
             config.resolve.alias
-                .set("@", path.join(__dirname, "./" + vueWorkDir))
-    }
+                .set("@", path.join(__dirname, "./" + vueWorkDir)),
+
+
+            config
+                .output
+                .filename('design/script/[name].[hash:8].js')
+                .chunkFilename('design/script/chunk[id].[hash:8].js')
+
+        config.module
+            .rule('images')
+            .test(/\.(png|jpe?g|gif|svg)(\?.*)?$/)
+            .use('url-loader')
+            .loader('url-loader')
+            .options({
+                limit: inlineLimit,
+                name: 'design/layout/image/[name].[hash:8].[ext]'
+            })
+
+        config.module.rule('svg').use('file-loader').options({
+            name: 'design/layout/image/[name].[hash:8].[ext]'
+        })
+
+        config.module
+            .rule('fonts')
+            .test(/\.(woff2?|eot|ttf|otf)(\?.*)?$/i)
+            .use('url-loader')
+            .loader('url-loader')
+            .options({
+                limit: inlineLimit,
+                name: 'design/style/font/[name].[hash:8].[ext]'
+            })
+    },
+
+    css: {
+        extract: {
+            filename: 'design/style/sheet/[name].[hash:8].css',
+            chunkFilename: 'design/style/sheet/[name].[hash:8].css'
+        },
+    },
 }
